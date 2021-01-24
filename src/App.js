ac6504c4aef939ca64a4af62d2801b54572b5eaf
Dreezy305 <68451591+Dreezy305@users.noreply.github.com>
2021-01-24 00:21:32 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+//import logo from "./logo.svg";//
+import "./App.css";
+import AutoCompleteText from "./AutoCompleteText";
+import countries from "./countries";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Component">
+          <div className="App-Component">
+            <AutoCompleteText items={countries} />
+            <br />
+            <br />
+            <AutoCompleteText items={["Hey", "You"]} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
